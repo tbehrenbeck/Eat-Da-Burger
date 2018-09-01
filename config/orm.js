04674db7bf536992d716f86901744357b55a0594
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-01 09:14:03 -0500

    File set up

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+//Import  MySQL connection. 
+var connection = require("../config/connection");
+
+//--code here--
+
+// selectAll()
+// insertOne()
+// updateOne()
+
+//-----
+
+//Export orm for the model
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Classic Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Impossible Veggie Burger");
+INSERT INTO burgers (burger_name) VALUES ("South n Peppa");
