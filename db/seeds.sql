22282ad90e41950532f8153a6001797b56d3db23
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-04 11:32:07 -0500

    editting layout and setting up handlebar partials

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ("Classic Burger");
-INSERT INTO burgers (burger_name) VALUES ("The Impossible Veggie Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
 INSERT INTO burgers (burger_name) VALUES ("South n Peppa");
 
 SELECT * FROM burgers;
