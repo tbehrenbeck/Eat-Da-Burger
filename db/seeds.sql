ed3067b437990bd337d7e35d8adaf90edf80a421
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-06 14:23:48 -0500

    media queries

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO items (name) VALUES ('Apples');
 INSERT INTO items (name) VALUES ('Pasta');
-INSERT INTO items (name, need) VALUES ('Sweet Potatoes', true);
-INSERT INTO items (name, need) VALUES ('Almond Milk', true);
-INSERT INTO items (name, need) VALUES ('Frozen Spinach', true);
+INSERT INTO items (name, need) VALUES ('Potatoes', true);
+INSERT INTO items (name, need) VALUES ('Milk', true);
+INSERT INTO items (name, need) VALUES ('Spinach', true);
 INSERT INTO items (name) VALUES ('Oats');
